1ff57e7d5fe396fe1adc95a7ed60371f5ca0f5a5
u733434 <lukasz.gorecki@lhsystems.com>
2020-02-06 09:04:11 +0100

    Add components and navigation

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+  <div className="jumbotron">
+    <h1>ADADsadasd adasd</h1>
+    <p>React, sadasd, dasdsa, </p>
+    <a href="/about">About</a>
+  </div>
+  );
+}
+
+export default HomePage;
